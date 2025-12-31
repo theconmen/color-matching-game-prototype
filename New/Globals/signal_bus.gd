f05bfa916e7edb68ba22679f193e14c1a4980826
theconmen <ckayoung22@gmail.com>
2025-12-31 16:28:04 -0500

    Future new folder project creation

---
+++ New/Globals/signal_bus.gd
@@ -0,0 +1,11 @@
+extends Node
+
+#Scene Navigation signals
+signal random_match_game_selected
+signal RGB_race_game_selected
+signal puzzle_race_game_selected
+signal quick_match_game_selected
+
+# UI scene
+signal exit_to_main_button_pressed()
+signal continue_game_button_pressed()
---
+++ New/Main/main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	#SignalBus.random_match_game_selected.connect(_on_random_match_game_selected)
+	#SignalBus.RGB_race_game_selected.connect(_on_rgb_race_game_selected)
+	#SignalBus.puzzle_race_game_selected.connect(_on_puzzle_race_game_selected)
+	#SignalBus.quick_match_game_selected.connect(_on_quick_match_game_selected)
