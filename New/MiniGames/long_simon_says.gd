2ff4378007de97ac5c19b348dc62381184013f18
theconmen <ckayoung22@gmail.com>
2026-01-02 10:30:09 -0500

    Idk

---
+++ New/MiniGames/long_simon_says.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# need to add some scaling for difficulty
+func generate_puzzle():
+	pass
