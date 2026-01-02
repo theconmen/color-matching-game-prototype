af5066d7e878fa6e09bbe590f173d9e609b6f0bd
theconmen <ckayoung22@gmail.com>
2026-01-02 10:27:04 -0500

    Set up basic color base scene

---
+++ New/Util/Components/color_base.gd
@@ -0,0 +1,23 @@
+extends AnimatableBody2D
+
+class_name ColorBase
+## used as the base for color changing or color clicking with other functionality to be added as components to this class
+
+@export var current_color: Color
+@onready var sprite_material = $Icon.material
+
+
+func _on_click_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_just_pressed("left_click"):
+		SignalBus.color_left_clicked.emit(self, current_color)
+	elif event.is_action_just_pressed("right_click"):
+		SignalBus.color_right_clicked.emit(self, current_color)
+
+
+## takes new color in as argument, sets shader to new color, then sends out color changed signal containing itself, old color, and new color
+func set_new_color(color: Color):
+	var old_color = current_color
+	current_color = color
+	sprite_material.set_shader_parameter("override_color", current_color)
+	SignalBus.color_changed.emit(self, old_color, current_color)
+	
--- New/Util/Globals/signal_bus.gd
+++ New/Util/Globals/signal_bus.gd
@@ -6,3 +6,8 @@ signal single_player_game_selected()
 # UI scene
 signal exit_to_main_button_pressed()
 signal continue_game_button_pressed()
+
+#GamePlay signals
+signal color_changed(object, old_color, new_color)
+signal color_left_clicked(object, color)
+signal color_right_clicked(object, color)
